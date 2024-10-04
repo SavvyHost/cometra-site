b1d23163e73622fa8120abdc97a2f6bb101fd97b
elkhamyyali <elkhamyyalimohamedali@gmail.com>
2024-10-05 01:22:08 +0300

    commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,25 +8,8 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-        "custom-gradient": "linear-gradient(82deg, #945E13 0%, #CFB758 100%)",
-      },
       fontFamily: {
-        'segoe': ['Segoe UI', 'sans-serif'],
-      },
-      fontSize: {
-        'special-offer': ['36px', {
-          lineHeight: '47.88px',
-        }],
-      },
-      fontWeight: {
-        'semi-bold': '600',  // Use string for fontWeight
-      },
-      textShadow: {
-        custom: '0.0625rem 0 0.625rem #000',
+        dinCondensed: ['"D-DIN Condensed"', 'sans-serif'], // Add D-DIN Condensed
       },
     },
   },
